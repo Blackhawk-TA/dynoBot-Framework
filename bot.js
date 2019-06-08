760502e48263cfc3bfa03fcbeb5c7937368f1af3
Blackhawk-TA <tkcblackhawk@gmail.com>
2019-06-08 23:56:26 +0200

    removed token

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const DiscordBot = require("./build/DiscordBot");
-
+const security = require("./cfg/security");
 const bot = new DiscordBot();
 
-bot.client.login("NTg2OTgwMjg0NjE3MTk1NTIx.XPv6GQ.okpOpqwdr0tSu1O8O3Pi_lCj8js");
+bot.client.login(security.token);
 bot.client.on("message").then(msg => {
 	console.log(msg)
 });
