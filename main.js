91b35a5691588e17f2cc8aa094ecfd1140859358
Blackhawk <36313322+Blackhawk-TA@users.noreply.github.com>
2019-06-08 03:00:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const BotFramework = require("./build/BotFramework");
-const Discord = require("./build/wrappers/messengers/Discord");
+const Discord = require("./build/messengers/Discord");
 
 const bot = new BotFramework(new Discord());
 
