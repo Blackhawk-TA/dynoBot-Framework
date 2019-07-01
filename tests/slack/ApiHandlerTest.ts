a30a60b0fb4e17b1913672c9139eef90570de8d7
D069765 <daniel.peters01@sap.com>
2019-07-01 14:37:05 +0200

    Added new testcase for ApiHandler

--- tests/slack/ApiHandlerTest.ts
+++ tests/slack/ApiHandlerTest.ts
@@ -12,6 +12,18 @@ describe("The function callMethod", function() {
 			assert.strictEqual(result.ok, false,  "The result is not ok.");
 			assert.strictEqual(result.error, "unknown_method", "An unknown method has been called.");
 			assert.strictEqual(result.req_method, methodName, "The invalid method name was returned correctly");
-		})
+		});
+	});
+
+	it("Calls a method with invalid parameters", function() {
+		//Arrange
+		let methodName: string = "rtm.connect";
+
+		//Act
+		return ApiHandler.callMethod(methodName, {}).then(result => {
+			//Assert
+			assert.strictEqual(result.ok, false,  "The result is not ok.");
+			assert.ok(result.error, "The api returned an error message.");
+		});
 	});
 });
