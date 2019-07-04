31553e22fb14cbf3d75e6f30d9ab40f7c5abcee0
Blackhawk-TA <tkcblackhawk@gmail.com>
2019-07-04 20:53:26 +0200

    Updated travis config & Unit tests

--- tests/discord/DiscordBotTest.ts
+++ tests/discord/DiscordBotTest.ts
@@ -1,9 +1,9 @@
 import {DiscordClient} from "../../src/wrappers/discord/DiscordClient";
 
 const {DiscordBot} = require("../../src/DiscordBot");
-const botConfig = require("../../../tests/bot-config");
 const Discord = require("discord.js");
 const assert = require("assert");
+const token = process.env.TRAVIS ? process.env.TRAVIS : require("../../../tests/bot-config").token.discord;
 
 beforeEach(function() {
 	this.client = new Discord.Client();
@@ -16,7 +16,7 @@ afterEach(function() {
 describe("The bot initialisation", function() {
 	it("Has a user which is not yet defined", function() {
 		//Act
-		new DiscordBot(botConfig.token.discord);
+		new DiscordBot(token);
 
 		//Assert
 		assert.strictEqual(this.client.user, null, "The user is not yet defined.");
@@ -26,7 +26,7 @@ describe("The bot initialisation", function() {
 describe("The getter", function() {
 	it("Has a getter which returns the wrapped client object", function() {
 		//Act
-		let Bot = new DiscordBot(botConfig.token.discord);
+		let Bot = new DiscordBot(token);
 
 		//Assert
 		assert.strictEqual(Bot.client instanceof DiscordClient, true, "The wrapped client object was returned.");
