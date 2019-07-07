ffb57fcbe4bceacaf01d7118d7c81b653bafc361
Blackhawk-TA <tkcblackhawk@gmail.com>
2019-07-07 21:33:58 +0200

    Added support for new EventHandler

--- src/SlackBot.ts
+++ src/SlackBot.ts
@@ -7,14 +7,16 @@ import {ErrorHandler} from "./utils/ErrorHandler";
 const WebSocket = require("ws");
 
 export class SlackBot implements IBot{
-	_connection: WebSocket;
+	private _connection: WebSocket;
+	private _client: IClient;
 
 	constructor(token: string) {
 		ApiHandler.callMethod("rtm.connect", {token: token}).then(response => {
 			if (response.ok) {
 				this._connection = new WebSocket(response.url);
+				this._client = new SlackClient(this._connection);
 			} else {
-				ErrorHandler.log("Could not connect to the websocket: " + response.error);
+				ErrorHandler.throwErrorMessage("Could not connect to the websocket: " + response.error);
 			}
 		}).catch(error => {
 			ErrorHandler.throwError(error);
@@ -22,6 +24,6 @@ export class SlackBot implements IBot{
 	}
 
 	getClient(): IClient {
-		return new SlackClient(this._connection);
+		return this._client;
 	}
 }
--- src/wrappers/slack/SlackClient.ts
+++ src/wrappers/slack/SlackClient.ts
@@ -3,13 +3,29 @@ import {IUser} from "../interfaces/IUser";
 import {EventEmitter} from "events";
 import {IServer} from "../interfaces/IServer";
 import {ErrorHandler} from "../../utils/ErrorHandler";
-import {SlackEvent} from "./SlackEvent";
-import {IEvent} from "../interfaces/IEvent";
+import {EventHandler} from "../../utils/EventHandler";
 
 export class SlackClient implements IClient {
 	private readonly _events: EventEmitter;
 	private readonly _user: IUser; //TODO set
 	private readonly _client: any;
+	private readonly _apiEvents = {
+		error: {
+			name: "onerror",
+			returnClass: Error, //TODO check if it shall be wrapped
+			isWrapped: false
+		},
+		message: {
+			name: "onmessage",
+			returnClass: null, //TODO check how it shall be wrapped
+			isWrapped: true
+		},
+		ready: {
+			name: "onopen",
+			returnClass: SlackClient,
+			isWrapped: true,
+		}
+	};
 
 	constructor(client: any) {
 		this._events = new EventEmitter();
@@ -30,7 +46,7 @@ export class SlackClient implements IClient {
 	}
 
 	registerEvent(name: string) :void {
-		let Event: IEvent = new SlackEvent(name);
+		let Event: EventHandler = new EventHandler(name, this._apiEvents);
 		let wrappedName: string = Event.getApiEventName();
 		this._client[wrappedName] = (object?: any) => {
 			let WrappedObject = Event.getWrappedObject(object);
