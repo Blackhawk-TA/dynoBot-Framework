8a3e11c6a13f0942681b85f05b41a50c591a770a
Blackhawk-TA <tkcblackhawk@gmail.com>
2019-07-07 23:09:17 +0200

    Adjusted ApiHandlerTest

--- tests/slack/ApiHandlerTest.ts
+++ tests/slack/ApiHandlerTest.ts
@@ -1,29 +1,31 @@
 const {ApiHandler} = require("../../src/wrappers/slack/ApiHandler");
 const assert = require("assert");
 
-describe("The function callMethod", function() {
-	it("Calls an invalid method", function () {
-		//Arrange
-		let methodName: string = "invalid.method";
+describe("The class ApiHandler", function() {
+	describe("The method callMethod", function() {
+		it("Calls an invalid method", function() {
+			//Arrange
+			let methodName: string = "invalid.method";
 
-		//Act
-		return ApiHandler.callMethod(methodName, {}).then(result => {
-			//Assert
-			assert.strictEqual(result.ok, false, "The result is not ok.");
-			assert.strictEqual(result.error, "unknown_method", "An unknown method has been called.");
-			assert.strictEqual(result.req_method, methodName, "The invalid method name was returned correctly");
+			//Act
+			return ApiHandler.callMethod(methodName, {}).then(result => {
+				//Assert
+				assert.strictEqual(result.ok, false, "The result is not ok.");
+				assert.strictEqual(result.error, "unknown_method", "An unknown method has been called.");
+				assert.strictEqual(result.req_method, methodName, "The invalid method name was returned correctly");
+			});
 		});
-	});
 
-	it("Calls a method with invalid parameters", function() {
-		//Arrange
-		let methodName: string = "rtm.connect";
+		it("Calls a method with invalid parameters", function() {
+			//Arrange
+			let methodName: string = "rtm.connect";
 
-		//Act
-		return ApiHandler.callMethod(methodName, {}).then(result => {
-			//Assert
-			assert.strictEqual(result.ok, false, "The result is not ok.");
-			assert.ok(result.error, "The api returned an error message.");
+			//Act
+			return ApiHandler.callMethod(methodName, {}).then(result => {
+				//Assert
+				assert.strictEqual(result.ok, false, "The result is not ok.");
+				assert.ok(result.error, "The api returned an error message.");
+			});
 		});
 	});
 });
