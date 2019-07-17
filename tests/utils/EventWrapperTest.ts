1aee0afd95ef5f9bf602d1763641ab72fb6f1591
Blackhawk-TA <tkcblackhawk@gmail.com>
2019-07-17 22:02:10 +0200

    Added unit tests for EventWrapper class

--- tests/utils/EventWrapperTest.ts
+++ tests/utils/EventWrapperTest.ts
@@ -1,7 +1,9 @@
 import {EventEmitter} from "events";
 import {EventWrapper} from "../../src/utils/EventWrapper";
+import {EventHandler} from "../../src/utils/EventHandler";
 
 const assert = require("assert");
+const sinon = require("sinon");
 
 describe("The class EventWrapper", function() {
 	describe("The method registerEvents", function() {
@@ -23,28 +25,36 @@ describe("The class EventWrapper", function() {
 			};
 			this.wrappedEmitter = new EventEmitter();
 			this.EventWrapper = new EventWrapper(this.originalEmitter, this.wrappedEmitter);
+			this.wrapStub = sinon.stub(EventHandler.prototype, "wrap");
 		});
 
 		afterEach(function() {
 			this.originalEmitter = null;
 			this.wrappedEmitter = null;
 			this.EventEmitter = null;
+			this.wrapStub.restore();
 		});
 
-		/*it("Registers all events including init events", function() {
+		it("Registers all events including init events", function() {
 			//Act
 			this.EventWrapper.registerEvents(this.eventsToRegister);
 
-			this.originalEmitter.emit("ready");
-			this.originalEmitter.emit("guildMemberAdd");
+			//Assert
+			assert.strictEqual(this.wrapStub.callCount, 2, "The wrap method has been called twice.");
+			assert.strictEqual(this.wrapStub.getCall(0).args[0], this.originalEmitter, "The correct emitter was passed.");
+			assert.strictEqual(this.wrapStub.getCall(0).args[1], this.wrappedEmitter, "The correct emitter was passed.");
+			assert.strictEqual(this.wrapStub.getCall(1).args[0], this.originalEmitter, "The correct emitter was passed.");
+			assert.strictEqual(this.wrapStub.getCall(1).args[1], this.wrappedEmitter, "The correct emitter was passed.");
+		});
+
+		it("Registers all events excluding init events", function() {
+			//Act
+			this.EventWrapper.registerEvents(this.eventsToRegister, true);
 
 			//Assert
-			return new Promise(resolve => {
-				this.wrappedEmitter.on("serverMemberAdd", () => {
-					assert.strictEqual(this.wrappedEmitter, 2, "The correct amount of events was wrapped.");
-					resolve();
-				});
-			});
-		});*/
+			assert.strictEqual(this.wrapStub.callCount, 1, "The wrap method has been called twice.");
+			assert.strictEqual(this.wrapStub.getCall(0).args[0], this.originalEmitter, "The correct emitter was passed.");
+			assert.strictEqual(this.wrapStub.getCall(0).args[1], this.wrappedEmitter, "The correct emitter was passed.");
+		});
 	});
 });
