5c97811338c6a3b2c0c90ffe56e2e3906e13d5a1
Blackhawk <daniel@tapadventures.com>
2020-06-06 23:31:05 +0200

    Build for v1.3.0

---
+++ build/wrappers/discord/DiscordClient.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DiscordClient = void 0;
+const DiscordUser_1 = require("./DiscordUser");
+const DiscordServer_1 = require("./DiscordServer");
+class DiscordClient {
+    constructor(client) {
+        this._user = new DiscordUser_1.DiscordUser(client);
+        this._client = client;
+    }
+    getUser() {
+        return this._user;
+    }
+    getServers() {
+        let servers = this._client.guilds.cache.array();
+        let wrappedServers = [];
+        servers.forEach(server => {
+            wrappedServers.push(new DiscordServer_1.DiscordServer(server));
+        });
+        return wrappedServers;
+    }
+}
+exports.DiscordClient = DiscordClient;
+//# sourceMappingURL=DiscordClient.js.map
