28a18e1ca114c8b90bdf2e028564f64d57e57810
Blackhawk <daniel.peters1@pm.me>
2021-04-30 17:07:34 +0200

    Build for v1.3.4

---
+++ build/utils/ErrorHandler.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ErrorHandler = void 0;
+class ErrorHandler {
+    /**
+     * Converts the given error message to an error, throws it and quits the program
+     * @param errorMessage - The message which shall be converted to an error object
+     */
+    static throwErrorMessage(errorMessage) {
+        let error = new Error(errorMessage);
+        throw error;
+    }
+    /**
+     * Throws the given error and quits the program
+     * @param error - The error which shall be thrown
+     */
+    static throwError(error) {
+        throw error;
+    }
+}
+exports.ErrorHandler = ErrorHandler;
+//# sourceMappingURL=ErrorHandler.js.map
